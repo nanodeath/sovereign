aa0340a407c674f82f7e7074273ea14af68df7c1
Max Aller <nanodeath@gmail.com>
2008-12-25 18:14:21 -0800

    Lots of changes, login and register working now

--- myapp.rb
+++ myapp.rb
@@ -1,9 +1,109 @@
 require 'rubygems'
 $:.unshift File.dirname(__FILE__) + '/sinatra/lib'
+$:.unshift File.dirname(__FILE__) + '/couchrest/lib'
 require 'sinatra'
+require 'couchrest'
+require 'digest/sha1'
+
+module RFC822
+  EmailAddress = begin
+    qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+    dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+    atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
+      '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+    quoted_pair = '\\x5c[\\x00-\\x7f]'
+    domain_literal = "\\x5b(?:#{dtext}|#{quoted_pair})*\\x5d"
+    quoted_string = "\\x22(?:#{qtext}|#{quoted_pair})*\\x22"
+    domain_ref = atom
+    sub_domain = "(?:#{domain_ref}|#{domain_literal})"
+    word = "(?:#{atom}|#{quoted_string})"
+    domain = "#{sub_domain}(?:\\x2e#{sub_domain})*"
+    local_part = "#{word}(?:\\x2e#{word})*"
+    addr_spec = "#{local_part}\\x40#{domain}"
+    pattern = /\A#{addr_spec}\z/
+  end
+end
+
+configure do
+  enable :sessions
+end
 
 get '/' do
   haml :index
 end
 
+db = CouchRest.database('http://localhost:5984/sovereign')
 
+helpers do
+  def reset_logged_in(duration=60*15)
+    set_cookie('logged_in', {:value => 'true', :expires => Time.new + duration})
+  end  
+end
+
+get '/css/*.sass' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass "/sass/#{params[:splat].first}".to_sym
+end
+
+post '/session/new' do
+  failure = nil  
+  if params[:login] == '' || params[:password] == ''
+    # Username or password not given
+    failure = "no_user_or_password_given"
+  else
+    result = db.view("frontend/all_users", {:key => params[:login]})['rows'].first
+    if result.nil?
+      # User not found
+      if(params[:login] =~ /[^a-zA-Z]/)
+        failure = 'invalid_user'
+      else
+        failure = "user_not_found"
+      end
+      
+    elsif Digest::SHA1.hexdigest(params[:password] + result['value']['salt'].to_s) != result['value']['password_hash']
+      # User found, but wrong password
+      failure = "wrong_password"
+    else
+      # User found, and right password
+      set_cookie("user_id", result['id'])
+      set_cookie("user_username", result['value']['username'])
+      reset_logged_in
+    end
+  end
+  
+  if failure.nil?
+    "{'operation': 'login', 'status': 'ok'}"
+  else
+    "{'operation': 'login', 'status': 'fail', 'message': '#{failure}'}"
+  end
+end
+
+post '/user/new' do
+  failure = nil
+  if params[:email].empty?
+    failure = 'no_email'
+  elsif not params[:email] =~ RFC822::EmailAddress
+    failure = 'invalid_email'
+  elsif(params[:login] =~ /[^a-zA-Z]/ or false) # s/false/invalid password/
+    failure = 'tampering'
+  else
+    # Valid email
+    salt = '1234'
+    password_hash = Digest::SHA1.hexdigest(params[:password] + salt)
+    db.save({:type => 'user', :username => params[:login], :salt => salt, :password_hash => password_hash})
+  end
+  
+  if failure.nil?
+    "{'operation': 'register', 'status': 'ok'}"
+  else
+    "{'operation': 'register', 'status': 'fail', 'message': '#{failure}'}"
+  end  
+end
+
+get '/_*' do
+  haml "/partials/#{params[:splat].first}".to_sym, :layout => false
+end
+
+get '/kingdom/*' do
+  haml "/partials/kingdom/#{params[:splat].first}".to_sym, :layout => false
+end
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,103 @@
 $(function(){
-
+  var submitted_login;
+  var submitted_password;
+  var login = $.cookie('user_username');
+  
+  
+  function initializePage(){
+    if ($.cookie('logged_in') == 'true') {
+      console.info("you are logged in");
+      loadKingdom();
+    } else {
+      console.info('you are not logged in');
+      $("#canvas").load("/_login");
+    }
+  }
+  initializePage();
+  
+  function loadKingdom(page){
+    if (!page) page = 'your';
+    $("#canvas").load("/kingdom/" + page);
+  }
+  
+  $(document).ajaxComplete(function(event, xhr, ajaxOptions){
+    var setCookie = xhr.getResponseHeader('Set-Cookie');
+    if (setCookie != '') {
+      document.cookie = setCookie;
+    }
+    
+    $("#register_form").ajaxForm({
+      dataType: 'json',
+      success: function(response){
+        if (response.status == 'ok') {
+          $("#register_modal").jqmHide();
+          
+        } else {
+          alert("try again");
+        }
+      }
+    });
+    
+    $("#login_form").ajaxForm({
+      dataType: 'json',
+      beforeSubmit: function(){
+        submitted_login = $("#login").val();
+        submitted_password = $("#password").val();
+      },
+      success: function(response){
+        if (response.status == 'ok') {
+          $("#login_form").hide();
+          $("#login_message").html("<p>Login successful.</p>").removeClass('failure').addClass('success').show();
+          $("#login_form #login").val("");
+          setTimeout(function(){
+            loadKingdom();
+          }, 500);
+        } else {
+          var reason = "";
+          switch (response.message) {
+            case "no_user_or_password_given":
+              reason = $("<p>Please supply username and password</p>");
+              break;
+            case "invalid_user":
+              reason = $("<p>Invalid login.  Can <strong>only</strong> contain letters.</p>");
+              break;
+            case "user_not_found":
+              var link = $("<a href='#'>register</a>");
+              link.click(function(){
+                $("#register_login").val(submitted_login);
+                $("#register_password").val(submitted_password);
+                $("#register_modal").jqmShow();
+                $("#register_email").focus();
+              });
+              reason = $("<p>User not found.  Perhaps you'd like to </p>").append(link).append(" it?");
+              break;
+            case "wrong_password":
+              reason = $("<p>Invalid password</p>");
+              break;
+            default:
+              reason = $("<p>Unknown</p>");
+          }
+          var message = $("<p>Login failed.</p>").append(reason.prepend("Reason: "));
+          $("#login_message").html(message).removeClass('success').addClass('failure').slideDown('fast');
+        }
+      }
+    });
+    
+    $('#register_modal').jqm();
+    
+    if (login && $("#login_form #login").val() == '') {
+      $("#login_form #login").val(login);
+    }
+    
+    $("#sign_out").click(function(){
+      $.cookie('logged_in', null);
+      initializePage();
+    });
+    
+    $("#sign_out_completely").click(function(){
+      $.cookie('logged_in', null);
+      $.cookie('user_username', null);
+      initializePage();
+    });
+  });
 });
