ea11eba46967d7590a5e3b4d517e6d7ae5dcdc1d
Max Aller <nanodeath@gmail.com>
2009-01-18 13:36:59 -0800

    Little more work on PageManager.

--- public/js/main.js
+++ public/js/main.js
@@ -71,137 +71,137 @@ $(function(){
     }
   });
 
-  PageManager.bindPage('province_people', function(event, json){
-    $("#canvas").html(json.html);
-  });
+//  PageManager.bindPage('province_people', function(event, json){
+//    $("#canvas").html(json.html);
+//  });
 
-  PageManager.bindPage('province_all', function(event, json){
+//  PageManager.bindPage('province_all', function(event, json){
+//
+//  });
 
-  });
-
-  PageManager.bindOperation('province_header', 'ok', function(event, json){
-    $("#menu_bar").html(json.html);
-  })
+//  PageManager.bindOperation('province_header', 'ok', function(event, json){
+//    $("#menu_bar").html(json.html);
+//  })
   
-  PageManager.bindPage('province_land', function(event, json){
-    $("#canvas").html(json.html);
-    $(".buildings input[type='button'].add_to_queue").click(function(){
-      var data = $(".buildings input[type='text'].add_to_queue").serializeArray();
-      $.ajax({
-        type: 'POST',
-        url: '/province/land/buildings',
-        dataType: 'json',
-        data: {
-          build_queue: data,
-          province: province
-        },
-        success: function(response){
-          if(response.operation){
-            PageManager.triggerOperation(response.operation, response.status, response);
-          }
-        }
-      });
-    });
-    $("#build_queue").sortable();
+//  PageManager.bindPage('province_land', function(event, json){
+//    $("#canvas").html(json.html);
+//    $(".buildings input[type='button'].add_to_queue").click(function(){
+//      var data = $(".buildings input[type='text'].add_to_queue").serializeArray();
+//      $.ajax({
+//        type: 'POST',
+//        url: '/province/land/buildings',
+//        dataType: 'json',
+//        data: {
+//          build_queue: data,
+//          province: province
+//        },
+//        success: function(response){
+//          if(response.operation){
+//            PageManager.triggerOperation(response.operation, response.status, response);
+//          }
+//        }
+//      });
+//    });
+//    $("#build_queue").sortable();
+//
+//    $("div.buildings img.building_image").each(function(){
+//
+//      // Add data to the construction targets about buildings that can be built
+//      // on them
+//      var build_targets = $(this).attr("can_be_built_on").split(',');
+//      var construction_targets = $(".construction_targets");
+//      for(var i = 0, len = build_targets.length; i < len; i++){
+//        var target = construction_targets.find("img[building="+build_targets[i]+"]");
+//        var current = target.attr("buildable");
+//
+//        current = current ? current.split(',') : [];
+//
+//        var b = $(this).attr("building");
+//        if(b){
+//          current.push(b);
+//          target.attr("buildable", current);
+//        }
+//      }
+//
+//      $(this).draggable({
+//        revert: 'invalid',
+//        start: function(){
+//          $("div.construction_targets").addClass('drag_in_process');
+//          console.log($(this));
+//        },
+//        stop: function(){
+//          $("div.construction_targets").removeClass('drag_in_process');
+//        },
+//        helper: function(){
+//          return $(this).clone();
+//        },
+//        opacity: '0.5'
+//      });
+//    });
+//
+//    $("div.construction_targets img.building_image").each(function(){
+//      var buildable = $(this).attr('buildable');
+//      if(buildable){
+//        buildable = buildable.split(',');
+//        var buildable_arr = [];
+//        for(var i = 0, len = buildable.length; i < len; i++){
+//          buildable_arr.push("div.buildings [building=" + buildable[i] + "]");
+//        }
+//        buildable = buildable_arr.join(',');
+//        console.info("buildable for " + $(this).attr("building") + " is ")
+//        console.info($(buildable));
+//        $(this).droppable({
+//          accept: buildable,
+//          activeClass: 'available_build_target',
+//          hoverClass: 'current_target_valid',
+//          drop: function(ev, ui){
+//            console.log('dropped');
+//            //            console.log(ev.target)
+//            //            console.log(ui);
+//            var clone = ui.draggable.clone();
+//            $("#build_queue").append(clone);
+//            var position = clone.position();
+//            clone.remove();
+//            var anim = {
+//              left: position.left,
+//              top: position.top
+//            };
+//            var bImage = $(ev.target).attr("src");
+//
+//            cloneDiv = $("<div />").css({
+//              width: 64,
+//              height: 64,
+//              position: 'absolute',
+//              left: ui.position.left,
+//              top: ui.position.top,
+//              "background-image": "url(" + bImage + ")",
+//              display: 'inline-block'
+//            }).append(ui.draggable.clone())
+//            .insertAfter(ui.draggable)
+//            .animate(anim, 1000, function(){
+//              cloneDiv.appendTo("#build_queue");
+//              cloneDiv.css({
+//                position: "",
+//                left: "",
+//                top: ""
+//              });
+//            });
+//          }
+//        });
+//      }
+//    });
+//
+//    (new Image()).src = 'http://www.google.com/intl/en_ALL/images/logo.gif';
+//  });
 
-    $("div.buildings img.building_image").each(function(){
+//  PageManager.bindPage('province_summary', function(event, json){
+//    $("#canvas").html(json.html)
+//  });
 
-      // Add data to the construction targets about buildings that can be built
-      // on them
-      var build_targets = $(this).attr("can_be_built_on").split(',');
-      var construction_targets = $(".construction_targets");
-      for(var i = 0, len = build_targets.length; i < len; i++){
-        var target = construction_targets.find("img[building="+build_targets[i]+"]");
-        var current = target.attr("buildable");
-
-        current = current ? current.split(',') : [];
-
-        var b = $(this).attr("building");
-        if(b){
-          current.push(b);
-          target.attr("buildable", current);
-        }
-      }
-
-      $(this).draggable({
-        revert: 'invalid',
-        start: function(){
-          $("div.construction_targets").addClass('drag_in_process');
-          console.log($(this));
-        },
-        stop: function(){
-          $("div.construction_targets").removeClass('drag_in_process');
-        },
-        helper: function(){
-          return $(this).clone();
-        },
-        opacity: '0.5'
-      });
-    });
-
-    $("div.construction_targets img.building_image").each(function(){
-      var buildable = $(this).attr('buildable');
-      if(buildable){
-        buildable = buildable.split(',');
-        var buildable_arr = [];
-        for(var i = 0, len = buildable.length; i < len; i++){
-          buildable_arr.push("div.buildings [building=" + buildable[i] + "]");
-        }
-        buildable = buildable_arr.join(',');
-        console.info("buildable for " + $(this).attr("building") + " is ")
-        console.info($(buildable));
-        $(this).droppable({
-          accept: buildable,
-          activeClass: 'available_build_target',
-          hoverClass: 'current_target_valid',
-          drop: function(ev, ui){
-            console.log('dropped');
-            //            console.log(ev.target)
-            //            console.log(ui);
-            var clone = ui.draggable.clone();
-            $("#build_queue").append(clone);
-            var position = clone.position();
-            clone.remove();
-            var anim = {
-              left: position.left,
-              top: position.top
-            };
-            var bImage = $(ev.target).attr("src");
-            
-            cloneDiv = $("<div />").css({
-              width: 64,
-              height: 64,
-              position: 'absolute',
-              left: ui.position.left,
-              top: ui.position.top,
-              "background-image": "url(" + bImage + ")",
-              display: 'inline-block'
-            }).append(ui.draggable.clone())
-            .insertAfter(ui.draggable)
-            .animate(anim, 1000, function(){
-              cloneDiv.appendTo("#build_queue");
-              cloneDiv.css({
-                position: "",
-                left: "",
-                top: ""
-              });
-            });
-          }
-        });
-      }
-    });
-
-    (new Image()).src = 'http://www.google.com/intl/en_ALL/images/logo.gif';
-  });
-
-  PageManager.bindPage('province_summary', function(event, json){
-    $("#canvas").html(json.html)
-  });
-
-  PageManager.bindOperation('add_to_building_queue', 'ok', function(event, response){
-    $("div.buildings").html(response.html);
-    PageManager.triggerPage('province_land');
-  });
+//  PageManager.bindOperation('add_to_building_queue', 'ok', function(event, response){
+//    $("div.buildings").html(response.html);
+//    PageManager.triggerPage('province_land');
+//  });
 
   PageManager.bindPage('frontpage', function(){
     $("#register_form").ajaxForm({
